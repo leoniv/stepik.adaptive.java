fe75bc74c34ff609272c581aeeafad7a1d580840
Leonid Vlasov <leoniv.vlasov@gmail.com>
2019-10-18 09:42:18 +0000

    Add lesson/spell_checker/Mian.java

---
+++ src/main/java/stepik/adaptive/lesson/spell_checker/Main.java
@@ -0,0 +1,21 @@
+package stepik.adaptive.lesson.spell_checker;
+
+import java.util.List;
+
+public class Main {
+  public static void main(String[] argv) {
+
+  }
+
+  static class Checker {
+    private List<String> knownWords;
+
+    public Checker(List<String> knownWords) {
+      this.knownWords = knownWords;
+    }
+
+    public List<String> spell(List<String> words) {
+      return null;
+    }
+  }
+}
