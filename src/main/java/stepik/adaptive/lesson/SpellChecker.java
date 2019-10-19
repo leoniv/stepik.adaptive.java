404f13ec9168d84f9dcba1313a484c46e5e5bcc6
Leonid Vlasov <leoniv.vlasov@gmail.com>
2019-10-19 22:53:11 +0700

    Complete SpellCheker

--- src/main/java/stepik/adaptive/lesson/SpellChecker.java
+++ src/main/java/stepik/adaptive/lesson/SpellChecker.java
@@ -1,21 +1,65 @@
 package stepik.adaptive.lesson;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import stepik.adaptive.util.IoReader;
 
 public class SpellChecker {
   public static void main(String[] argv) {
+    Input input = Input.parse(System.in);
+    input.lines.stream().flatMap(
+          line -> new Checker(input.knownWords).spell(line).stream()
+          ).sorted().forEach(s -> System.out.println(s));
+  }
 
+  static class Input {
+    List<String> knownWords;
+    List<String> lines;
+
+    static Input parse(InputStream inputStream) {
+      return parseLines(
+          IoReader.scannerFor(IoReader.scanLinesFunction()).apply(inputStream)
+          );
+    }
+
+    private static Input parseLines(List<String> inputLines) {
+      Integer kwCount = Integer.decode(inputLines.get(0));
+      List<String> knownWords = inputLines.subList(1, kwCount + 1);
+      List<String> lines = inputLines.subList(kwCount + 2, inputLines.size());
+      return new Input(lines, knownWords);
+    }
+
+    private Input(List<String> lines, List<String> knownWords) {
+      this.lines = lines;
+      this.knownWords = knownWords;
+    }
   }
 
   static class Checker {
-    private List<String> knownWords;
+    private HashSet<String> knownWords;
 
     public Checker(List<String> knownWords) {
-      this.knownWords = knownWords;
+      this.knownWords = new HashSet<String>(knownWords.size());
+      knownWords.forEach(s-> this.knownWords.add(s.toLowerCase()));
     }
 
     public List<String> spell(List<String> words) {
-      return null;
+      return words.stream()
+               .filter(s -> ! this.knownWords.contains(s.toLowerCase()))
+               .collect(Collectors.toList());
+    }
+
+    public List<String> spell(String str) {
+      return spell(words(str));
+    }
+
+    public static List<String> words(String str) {
+      return IoReader.scannerFor(IoReader.scanWordsFunction())
+        .apply(new ByteArrayInputStream(str.getBytes()));
     }
   }
 }
--- src/test/groovy/stepik/adaptive/lesson/SpellCheckerSpec.groovy
+++ src/test/groovy/stepik/adaptive/lesson/SpellCheckerSpec.groovy
@@ -1,23 +1,53 @@
 package stepik.adaptive.lesson;
 
-import spock.lang.*;
-import stepik.adaptive.lesson.SpellChecker;
+import java.io.ByteArrayInputStream
+import spock.lang.*
+import stepik.adaptive.lesson.SpellChecker
 
 class SpellCheckerSpec extends Specification {
 
   static class CheckerSpec extends SpellCheckerSpec {
 
-     def "SpellChecker.Checker#spell"() {
+     @Unroll
+     def "SpellChecker.Checker#spell #words #knowWords"() {
        expect:
          assert new SpellChecker.Checker(knowWords).spell(words) == out
+         assert new SpellChecker.Checker(knowWords)
+           .spell(words.join(' ')) == out
 
        where:
          words                    | knowWords          | out
-         ['aA', 'aa', 'bb', 'cc'] | ['aa', 'bB', 'ee'] | ['cc', 'ee']
-         ['cc', 'aa', 'cc', 'aa'] | ['Aa', 'bb', 'ee'] | ['cc']
+         ['aA', 'aa', 'bb', 'cc'] | ['aa', 'bB', 'ee'] | ['cc']
+         ['cc', 'aa', 'cc', 'aa'] | ['Aa', 'bb', 'ee'] | ['cc', 'cc']
          ['cc', 'ee', 'cc', 'aa'] | ['aa', 'bb', 'ee'] | ['cc', 'cc']
-         ['c', 'b', 'e']          | []                 | ['c', 'b', 'c']
+         ['c', 'b', 'e']          | []                 | ['c', 'b', 'e']
          []                       | ['aa', 'bb', 'ee'] | []
+     }
+
+     def "SpellChecker.Input.parse()"() {
+       given:
+         def parser = SpellChecker.Input
+
+       when:
+         def parsed = parser.parse(input)
+
+       then:
+         assert parsed.knownWords == ['aa', 'bb', 'cc']
+         assert parsed.lines == ['aa bb cc', 'bb cc dd', 'cc dd ee']
+
+       where:
+         input = new ByteArrayInputStream(
+           '''
+           3
+           aa
+           bb
+           cc
+           3
+           aa bb cc
+           bb cc dd
+           cc dd ee
+           '''.trim().split("\n").collect({ it.trim() }).join("\n").bytes
+         )
      }
   }
 }
