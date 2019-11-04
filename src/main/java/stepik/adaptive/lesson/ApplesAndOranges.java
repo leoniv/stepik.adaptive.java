183e2a375273d2b113d095138a67b1dcda0911cf
Leonid Vlasov <leoniv.vlasov@gmail.com>
2019-11-04 20:12:28 +0700

    class ApplesAndOranges v3 tests pass

--- src/main/java/stepik/adaptive/lesson/ApplesAndOranges.java
+++ src/main/java/stepik/adaptive/lesson/ApplesAndOranges.java
@@ -1,6 +1,6 @@
 package stepik.adaptive.lesson;
 
-abstract class Fruit<T extends Fruit> implements Comparable<T> {
+abstract class Fruit {
 
   protected final int weigth;
 
@@ -12,30 +12,35 @@ abstract class Fruit<T extends Fruit> implements Comparable<T> {
     return this.weigth;
   }
 
-  public int compareTo(T other) {
+  protected <T extends Fruit> int _compareTo(T other) {
     return Integer.compare(this.weigth, other.weigth);
   }
 }
 
-class Apple extends Fruit<Apple> {
+class Apple extends Fruit implements Comparable<Apple> {
 
   public Apple(int weight) {
     super(weight);
   }
 
+  public int compareTo(Apple other) { return _compareTo(other); }
+
 }
 
-class Orange extends Fruit<Orange> {
+class Orange extends Fruit implements Comparable<Orange> {
 
   public Orange(int weight) {
     super(weight);
   }
+
+  public int compareTo(Orange other) { return _compareTo(other); }
+
 }
 
 class Test {
   void test() {
     Fruit fruitApple = new Apple(30);
     Fruit fruitOrange = new Orange(40);
-    fruitApple.compareTo(fruitOrange);
+  //  fruitApple.compareTo(fruitOrange);
   }
 }
